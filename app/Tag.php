034b29234612481faea31b356c7137325adef6fa
David <investmentracker1@gmail.com>
2020-03-02 00:07:40 -0800

    master merging

--- app/Tag.php
+++ app/Tag.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     // Fetches all the articles belonging to its specific tag name.
+    // Testing master merging
     public function articles() {
         return $this->belongsToMany(Article::class);
     }
