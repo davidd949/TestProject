2a34fd64749e8e0f24868148e3485e4aba9bf0d8
David <investmentracker1@gmail.com>
2020-03-02 21:26:01 -0800

    master trying to merge into iss2

--- app/Tag.php
+++ app/Tag.php
@@ -8,6 +8,7 @@ class Tag extends Model
 {
     // Fetches all the articles belonging to its specific tag name.
     // ISS2 here with master merge conflict
+    // master merging into iss2
     public function articles() {
         return $this->belongsToMany(Article::class);
     }
