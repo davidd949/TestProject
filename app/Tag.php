52b7f7c3bf1d2e58d9a6f34d851a0841c78655d3
David <investmentracker1@gmail.com>
2020-03-02 21:21:19 -0800

    iss2 at it again

--- app/Tag.php
+++ app/Tag.php
@@ -8,6 +8,7 @@ class Tag extends Model
 {
     // Fetches all the articles belonging to its specific tag name.
     // ISS2 here with master merge conflict
+    // this is iss2 again...
     public function articles() {
         return $this->belongsToMany(Article::class);
     }
